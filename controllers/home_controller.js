93a6fb6b77911628152d5625a422b776879b3d1a
Arnab Banerjee <arnab@Arnabs-MacBook-Pro.local>
2020-07-08 12:49:52 +0530

    Display post and related user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,26 @@
-module.exports.home = function(request, response){
-    console.log(request.cookies);
-    return response.render('home', {title: "Home"});
-}
+const Post = require('../models/post');
 
-//module.exports.actionName = function(request, response){}
+module.exports.home = function(request, response){
+    // console.log(request.cookies);
+
+    // populate is used so that each post can have the name of the user
+    // this is ued to fetch the details of user from db by id
+    Post.find({}).populate('user').exec(function(err, post){
+        if(err){
+            console.log("Error in populating", err)
+        }
+        return response.render('home', {
+            title: "Codeial | Home",
+            post: post
+        });
+    });
+    // Post.find({}, function(err, post){
+    //     console.log('post', post);
+    //     return response.render('home',{
+    //         title: "Codeial | Home",
+    //         post: post
+    //     })
+    // })
+};
+
+//module.exports.actionName = function(request, response){}open browser
--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -3,7 +3,7 @@ const Post = require('../models/post');
 module.exports.create = function(request, response){
     Post.create({
         content: request.body.content,
-        user: request.body._id
+        user: request.user._id
     },function(err, post){
         if(err){
             console.log('Error in creating post');
